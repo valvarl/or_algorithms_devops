70f1a983f58637d8b4f19be1c344ce0ee9f2b894
Valeriy Varlachev <valvarl@ya.ru>
2022-06-04 20:14:28 +0300

    cpplint

--- myaction/entrypoint.sh
+++ myaction/entrypoint.sh
@@ -6,6 +6,7 @@ echo "${folders}"
 for folder in $folders; do # Not recommended, will break on whitespace
     echo $folders
       if [[ -d ${folder} ]]; then
+        echo `pwd`
         command="cpplint --recursive --exclude=*/test.cpp --filter=-legal/copyright,-build/include_subdir --linelength=120 ${folder}/*"
         echo "${command}"
         if ${command} | grep "error"; then
